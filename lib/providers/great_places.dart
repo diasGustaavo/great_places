c47b48789c7b81758d32607ce229e3bc3f241f37
Gustavo Dias <gustavodias@cc.ci.ufpb.br>
2022-09-26 16:13:42 -0300

    add places to the local storage using sqflite

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,9 +3,29 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:great_places/models/place.dart';
+import 'package:great_places/utils/db_util.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
+
+  Future<void> loadPlaces() async {
+    final dataList = await DbUtil.getData('places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: PlaceLocation(
+              latitude: 1.0,
+              longitude: 1.0,
+              address: 'teste',
+            ),
+          ),
+        )
+        .toList();
+    notifyListeners();
+  }
 
   List<Place> get items {
     return [..._items];
@@ -32,6 +52,11 @@ class GreatPlaces with ChangeNotifier {
     );
 
     _items.add(newPlace);
+    DbUtil.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
     notifyListeners();
   }
 }
