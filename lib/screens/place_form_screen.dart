f4accaf6dea04da359e7b4b8f27319dc3a114f27
Gustavo Dias <gustavodias@cc.ci.ufpb.br>
2022-09-26 16:48:14 -0300

    get location info using the location package

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/widgets/image_input.dart';
+import 'package:great_places/widgets/location_input.dart';
 import 'package:provider/provider.dart';
 
 class PlaceFormScreen extends StatefulWidget {
@@ -54,6 +55,8 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
                     ),
                     SizedBox(height: 10),
                     ImageInput(this._selectedImage),
+                    SizedBox(height: 10),
+                    LocationInput(),
                   ],
                 ),
               ),
@@ -70,7 +73,7 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
               style: TextStyle(color: Colors.black),
             ),
             style: ElevatedButton.styleFrom(
-              primary: Theme.of(context).colorScheme.secondary,
+              backgroundColor: Theme.of(context).colorScheme.secondary,
               elevation: 0,
               tapTargetSize: MaterialTapTargetSize.shrinkWrap,
             ),
